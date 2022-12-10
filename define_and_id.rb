9a5d23c6b1da9a38e35bdc9a1267641362cb3ea7
Caroline Peri <caroline.anne.peri@gmail.com>
2022-12-10 14:12:10 -0500

    Complete exercises and add definitions

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,26 @@
 
 
 "Hello World".include?("Hello")
-
+# The include? method is called on the string object "Hello World"
+# The argument "Hello", a string object, is passed. The include method checks to see if this argument string exists in the original object string.
+# Since the argument does exist, the return value is "true" (a Boolean).
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is called on the string object "Hello World", with one String argument ("Hello"), passed in.
+# This method checks to see if the String ends with the argument given.
+# Since the String does not end with "Hello", the return value is "false" (a Boolean).
 
 "Hello World".end_with?("rld")
-
+# The end_with? method is called on the string object "Hello World", with one String argument ("rld"), passed in.
+# This method checks to see if the String ends with the argument given.
+# Since the String does end with "rld", the return value is "true" (a Boolean).
 
 12.even?
-
+# The even? method is called on the integer object 12.
+# No arguments are passed. The even method evaluates whether the integer is even or odd.
+# Since 12 is even, the method returns the Boolean value "true"
 
 18.next
-
-
+# The next method is called on the integer object 18.
+# No arguments are passed. The next method returns the following integer for a given integer value.
+# Since 19 is the integer that follows the given object, the method returns the integer "19".
